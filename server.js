baaaca3449ddeefd280fde66c936a3518fb89011
Anton <antongowthaman@googlemail.com>
2022-01-03 12:21:49 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Web Application Currency Converter started and Listening on port 8080");
+});
+
+// serve css as static
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
